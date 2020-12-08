b0a3fd3820f606a2168c729fb2f24b4047080950
Jailton <jailtonsouza7@gmail.com>
2020-12-08 14:30:06 -0300

    Registering profile on admin

--- duplas/admin.py
+++ duplas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from duplas.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'sexo')
+    list_display = ('user', 'sexo')
