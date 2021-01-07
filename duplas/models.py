61021acb7963d5bfe62e8a8f238ef37a3d8ac7e6
Jailton <jailtonsouza7@gmail.com>
2021-01-07 17:50:41 -0300

    changing project structure

--- duplas/models.py
+++ duplas/models.py
@@ -2,34 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
+class Integrante(models.Model):
     SEXOS = (
+        ('', 'Selecione'),
         ('M', 'Masculino'),
         ('F', 'Feminino'),
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profiles')
-    sexo = models.CharField(max_length=22, choices=SEXOS)
+    name = models.CharField(max_length=127, verbose_name="Nome")
+    surname = models.CharField(max_length=127, verbose_name="Sobrenome", null=True, blank=True)
+    gender = models.CharField(max_length=20, verbose_name="Sexo", choices=SEXOS)
+    active = models.BooleanField(verbose_name='Ativo', default=True)
 
     def __str__(self):
-        return f'{self.user}'
+        if self.surname:
+            return f'{self.name} {self.surname}'
+        return f'{self.name}'
+
+    def get_full_name(self):
+        return self.__str__()
 
 
 class Duplas(models.Model):
     integrante_1 = models.ForeignKey(
-        Profile,
+        Integrante,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name="primeiro_integrante"
     )
     integrante_2 = models.ForeignKey(
-        Profile,
+        Integrante,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
         related_name="segundo_integrante"
     )
-    data = models.DateField(unique=True)
+    date = models.DateField(unique=True, verbose_name="Data")
 
     def __str__(self):
-        return f'{self.integrante_1.user.get_full_name()} e {self.integrante_2.user.get_full_name()}'
+        return f'{self.integrante_1.name} e {self.integrante_2.name}'
